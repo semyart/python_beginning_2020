13a7d0833e4eb45ea296bded09e5c680a7e10995
semyart <olsemykin@gmail.com>
2020-08-04 14:39:26 +0300

    Smile drawing

---
+++ lab3/ex3.py
@@ -0,0 +1,41 @@
+from graph import *
+
+#face
+penColor(0, 0, 0)
+brushColor("yellow")
+circle(250,250, 150)
+
+#mouth
+brushColor("black")
+rectangle(170, 350, 330, 320)
+
+#eyes
+brushColor("red")
+circle(170, 200, 25)
+circle(170, 200, 25)
+brushColor("black")
+circle(170, 200, 13)
+circle(170, 200, 13)
+
+brushColor("red")
+circle(330, 200, 25)
+circle(330, 200, 25)
+brushColor("black")
+circle(330, 200, 13)
+circle(330, 200, 13)
+
+#eyebrows
+penSize(20)
+line(100, 123, 230, 193)
+line(280, 193, 385, 130)
+
+
+
+
+
+
+
+
+
+
+run()
